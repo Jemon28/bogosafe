e9f889fb7a32b8ba61515485eb98bf420ed23e0e
Julen Rubio <jstevenrubio@unipanamericana.edu.co>
2020-04-27 20:43:49 -0500

    corrección proyecto

--- Codigo/BogoSafe/app/src/androidTest/java/co/edu/unipanamericana/bogosafe/ExampleInstrumentedTest.java
+++ Codigo/BogoSafe/app/src/androidTest/java/co/edu/unipanamericana/bogosafe/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package co.edu.unipanamericana.bogosafe;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
